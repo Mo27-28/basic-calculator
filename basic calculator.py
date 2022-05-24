ed07637b30a16b70dc531bad967049c9bb84a29e
Mo27-28 <105903339+Mo27-28@users.noreply.github.com>
2022-05-24 23:20:12 +0200

    Update basic calculator.py

--- basic calculator.py
+++ basic calculator.py
@@ -1,6 +1,16 @@
 #basic calculator code
-num1=float(input('enter the first number: '))
-num2=float(input('Enter second number: '))
+try: 
+  num1=float(input('enter the first number: '))
+except: 
+  print('value is not a number')
+  quit()
+
+try:
+  num2=float(input('Enter second number: '))
+except:
+  print('value is not a number')
+  quit()
+  
 operator = input('Enter operator: ')
 
 
